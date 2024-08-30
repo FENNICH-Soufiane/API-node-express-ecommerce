d16d29b37883ce7bd54900e7541bee2a92647937
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-08-30 19:26:53 +0100

    Error Handling

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dbConnect from "../config/dbConnect.js";
 import userRoutes from "../routes/usersRoute.js";
+import {globalErrhandler} from "../middlewares/globalErrHandler.js"
 
 dbConnect();
 
@@ -8,5 +9,6 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/v1/users/", userRoutes);
+app.use(globalErrhandler);
 
 export default app;
