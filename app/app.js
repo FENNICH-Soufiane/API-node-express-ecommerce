3cc42708d2090fbdfdb2577b011da5d19e9ebd37
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-08-30 19:42:19 +0100

    Not Found Route Handler

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dbConnect from "../config/dbConnect.js";
 import userRoutes from "../routes/usersRoute.js";
-import {globalErrhandler} from "../middlewares/globalErrHandler.js"
+import {globalErrhandler, notFound} from "../middlewares/globalErrHandler.js"
 
 dbConnect();
 
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/v1/users/", userRoutes);
+app.use(notFound);
 app.use(globalErrhandler);
 
 export default app;
