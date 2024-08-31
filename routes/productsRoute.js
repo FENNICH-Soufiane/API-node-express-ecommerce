10467c6f12822b8e2654ebe7a399d908004e46c0
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-08-31 20:50:14 +0100

    Update Product

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProductCtrl, getProductsCtrl, getProductCtrl } from "../controllers/productsCtrl.js";
+import { createProductCtrl, getProductsCtrl, getProductCtrl, updateProductCtrl } from "../controllers/productsCtrl.js";
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
 
 
@@ -8,6 +8,7 @@ const productsRoutes = express.Router();
 productsRoutes.post('/', isLoggedIn, createProductCtrl);
 productsRoutes.get('/', getProductsCtrl);
 productsRoutes.get('/:id', getProductCtrl);
+productsRoutes.put('/:id', updateProductCtrl);
 
 
 export default productsRoutes;
