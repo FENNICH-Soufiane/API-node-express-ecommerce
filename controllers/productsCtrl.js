6523e195bed845cac4b94938c06c9872e2b4a8b7
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-08-31 20:19:31 +0100

    Pagination Results

--- controllers/productsCtrl.js
+++ controllers/productsCtrl.js
@@ -105,10 +105,29 @@ export const getProductsCtrl = asyncHandler(async (req, res) => {
 
     productQuery = productQuery.skip(startIndex).limit(limit);
 
+    //pagination results
+    const pagination = {};
+    if (endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit,
+        };
+    }
+    if (startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit,
+        };
+    }
+
     const products = await productQuery;
 
     res.json({
         status: "success",
-        products
+        total,
+        results: products.length,
+        pagination,
+        message: "Products fetched successfully",
+        products,
     })
 })
