01a0db4cff25bd605e2c5edf5d46e6ee20487639
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-09-01 00:07:36 +0100

    Single, Update, Delete Categories Controller

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import dbConnect from "../config/dbConnect.js";
 import userRoutes from "../routes/usersRoute.js";
 import {globalErrhandler, notFound} from "../middlewares/globalErrHandler.js"
 import productsRoutes from "../routes/productsRoute.js";
+import categoriesRouter from "../routes/categoriesRouter.js";
 
 dbConnect();
 
@@ -11,7 +12,7 @@ app.use(express.json());
 
 app.use("/api/v1/users/", userRoutes);
 app.use("/api/v1/products/", productsRoutes);
-app.use("/api/v1/categories/", productsRoutes);
+app.use("/api/v1/categories/", categoriesRouter);
 
 app.use(notFound);
 app.use(globalErrhandler);
--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,9 +1,13 @@
 import exppress from "express";
-import { isLoggedIn } from "../middlewares/isLoggedIn";
-import { createCategoryCtrl } from "../controllers/categoriesCtrl";
+import { isLoggedIn } from "../middlewares/isLoggedIn.js";
+import { createCategoryCtrl, deleteCategoryCtrl, getAllCategoriesCtrl, getSingleCategoryCtrl, updateCategoryCtrl } from "../controllers/categoriesCtrl.js";
 
 const categoriesRouter = exppress.Router();
 
 categoriesRouter.post("/", isLoggedIn, createCategoryCtrl);
+categoriesRouter.get("/", getAllCategoriesCtrl);
+categoriesRouter.get("/:id", getSingleCategoryCtrl);
+categoriesRouter.delete("/:id", deleteCategoryCtrl);
+categoriesRouter.put("/:id", updateCategoryCtrl);
 
 export default categoriesRouter;
