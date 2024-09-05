b77b7b7f1b5765f6a989a1fdb5ca6a6df2ef4f02
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-09-05 17:15:38 +0100

    Single, Delete, Update Coupon Controller

--- routes/couponsRouter.js
+++ routes/couponsRouter.js
@@ -1,13 +1,15 @@
 import exppress from "express";
 import isAdmin from "../middlewares/isAdmin.js";
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
-import { createCouponCtrl, getAllCouponsCtrl } from "../controllers/couponCtrl.js";
+import { createCouponCtrl, deleteCouponCtrl, getAllCouponsCtrl, getCouponCtrl, updateCouponCtrl } from "../controllers/couponCtrl.js";
 
 const couponsRouter = exppress.Router();
 
 
 couponsRouter.post("/", isLoggedIn, createCouponCtrl);
 couponsRouter.get("/", getAllCouponsCtrl);
-
+couponsRouter.put("/update/:id", isLoggedIn, isAdmin, updateCouponCtrl);
+couponsRouter.delete("/delete/:id", isLoggedIn, isAdmin, deleteCouponCtrl);
+couponsRouter.get("/single", getCouponCtrl);
 
 export default couponsRouter;
